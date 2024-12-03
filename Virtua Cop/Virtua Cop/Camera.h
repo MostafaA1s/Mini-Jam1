87d82ddbbaef1183fdee9d0024dc657ceee65fc5
Fouad Elamoury <foad.osama@hotmail.com>
2024-12-03 10:14:22 +0200

    camera movement fix header

--- Virtua Cop/Virtua Cop/Camera.h
+++ Virtua Cop/Virtua Cop/Camera.h
@@ -1,16 +1,18 @@
 #pragma once
+#include <SFML/Graphics.hpp>
+
 class Camera
 {
-//private:
-//    sf::RenderWindow& window;
-//    sf::View view;
-//    sf::Vector2f defaultPosition;
-//    sf::Vector2f currentPosition;
-//    sf::Vector2f targetPosition;
-//    float transitionSpeed; // Speed of camera transitions
-//public:
-//	Camera(sf::RenderWindow& window, sf::Vector2f& defaultPosition, float transitionSpeed = 5.0f);
-//	//updating the camera focus
-//	void Update(float deltaTime, sf::Vector2f* enemyPosition);
+private:
+    sf::RenderWindow& window;
+    sf::View view;
+    sf::Vector2f defaultPosition;
+    sf::Vector2f currentPosition;
+    sf::Vector2f targetPosition;
+    float transitionSpeed; // Speed of camera transitions
+public:
+	Camera( sf::RenderWindow& window, const sf::Vector2f& defaultPosition,  float transitionSpeed);
+	//updating the camera focus
+	void Update(float deltaTime, sf::Vector2f* enemyPosition);
 };
 
